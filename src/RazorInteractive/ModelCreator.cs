9f30c805558f08bf278063b5236be7cb1599013c
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2021-05-31 23:04:30 -0400

    Migrated from a middleware to a kernel

--- src/RazorInteractive/ModelCreator.cs
+++ src/RazorInteractive/ModelCreator.cs
@@ -10,7 +10,33 @@ namespace RazorInteractive
     internal static class ModelCreator
     {
         public static IDictionary<string, object> CreateModelFromCurrentVariables(this Kernel kernel)
-            => CreateModelFromCurrentVariables(kernel as DotNetKernel);
+        {
+            var model = new ExpandoObject() as IDictionary<string, object>;
+
+            if (kernel is CompositeKernel compositeKernel)
+            {
+                foreach (var childKernel in compositeKernel.ChildKernels)
+                {
+                    var tempModel = childKernel.CreateModelFromCurrentVariables();
+
+                    foreach (var item in tempModel)
+                    {
+                        model.TryAdd(item.Key, item.Value);
+                    }
+                }
+            }
+            else if (kernel is DotNetKernel dotnetKernel)
+            {
+                var tempModel = dotnetKernel.CreateModelFromCurrentVariables();
+
+                foreach (var item in tempModel)
+                {
+                    model.TryAdd(item.Key, item.Value);
+                }
+            }
+
+            return model;
+        }
 
         private static IDictionary<string, object> CreateModelFromCurrentVariables(this DotNetKernel dotNetKernel)
             => dotNetKernel
--- src/RazorInteractive/RazorKernelExtension.cs
+++ src/RazorInteractive/RazorKernelExtension.cs
@@ -1,26 +1,37 @@
-﻿using System.CommandLine;
+﻿using System;
 using System.CommandLine.Invocation;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Html;
 using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.Formatting;
 
 namespace RazorInteractive
 {
-    public class RazorKernelExtension : IKernelExtension
+    public class RazorKernelExtension : IKernelExtension, IStaticContentSource
     {
-        public Task OnLoadAsync(Kernel kernel)
+        public string Name => "Razor";
+
+        public async Task OnLoadAsync(Kernel kernel)
         {
-            var razorCommand = new Command("#!razor", "Renders the code block as Razor markup.")
+            if (kernel is CompositeKernel compositeKernel)
             {
-                Handler = CommandHandler.Create<KernelInvocationContext>(context =>
-                {
-                    context.HandlingKernel.AddRazor();
-                })
-            };
+                compositeKernel.Add(new RazorKernel());
+            }
 
-            kernel.AddDirective(razorCommand);
+            kernel.UseRazor();
 
-            return Task.CompletedTask;
+            var message = new HtmlString(@"
+<details>
+    <summary>Renders the code block as Razor markup in dotnet-interactive notebooks.</summary>
+    <p>This extension adds a new kernel that can render Razor markdown.</p>
+</details>");
+
+            var formattedValue = new FormattedValue(
+                HtmlFormatter.MimeType,
+                message.ToDisplayString(HtmlFormatter.MimeType));
+
+            await kernel.SendAsync(new DisplayValue(formattedValue, Guid.NewGuid().ToString()));
         }
     }
 }
