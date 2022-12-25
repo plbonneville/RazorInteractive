99659c283b7ff9eb7ecfbde16d303fade1c85d10
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2022-12-25 15:31:01 -0500

    Typo

--- src/RazorInteractive/RazorMarkdown.cs
+++ src/RazorInteractive/RazorMarkdown.cs
@@ -3,4 +3,4 @@
 /// <summary>
 /// Type used to register a markdown formatter for Razor.
 /// </summary>
-internal sealed record RazorMarkdown(string Valu e);
+internal sealed record RazorMarkdown(string Value);
