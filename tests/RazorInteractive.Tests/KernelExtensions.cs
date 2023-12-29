552f73bb984305fcdbaf160a39668d48ae34a63c
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2023-12-29 15:08:48 -0500

    Update the projects to .NET 8.0

---
+++ tests/RazorInteractive.Tests/KernelExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.DotNet.Interactive.CSharp;
+using Microsoft.DotNet.Interactive.FSharp;
+using Microsoft.DotNet.Interactive.PackageManagement;
+
+namespace RazorInteractive.Tests;
+
+public static class KernelExtensions
+{
+    public static CSharpKernel UseNugetDirective(this CSharpKernel kernel, bool forceRestore = false)
+    {
+        kernel.UseNugetDirective((k, resolvedPackageReference) =>
+        {
+
+            k.AddAssemblyReferences(resolvedPackageReference
+                .SelectMany(r => r.AssemblyPaths));
+            return Task.CompletedTask;
+        }, forceRestore);
+
+        return kernel;
+    }
+
+    public static FSharpKernel UseNugetDirective(this FSharpKernel kernel, bool forceRestore = false)
+    {
+        kernel.UseNugetDirective((k, resolvedPackageReference) =>
+        {
+            var resolvedAssemblies = resolvedPackageReference
+                .SelectMany(r => r.AssemblyPaths);
+
+            var packageRoots = resolvedPackageReference
+                .Select(r => r.PackageRoot);
+
+
+            k.AddAssemblyReferencesAndPackageRoots(resolvedAssemblies, packageRoots);
+            return Task.CompletedTask;
+        }, forceRestore);
+
+        return kernel;
+    }
+}
